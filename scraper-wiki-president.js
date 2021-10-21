f18266f066f602716cd007e28ea2c12878dcd224
Piotr Gabara <piotr.gabara@goodylabs.com>
2021-10-21 11:23:12 +0200

    Scraper single president extraction

---
+++ scraper-wiki-president.js
@@ -0,0 +1,13 @@
+const rp = require("request-promise");
+const $ = require("cheerio");
+const url = "https://en.wikipedia.org/wiki/George_Washington";
+
+rp(url)
+  .then(function (html) {
+    console.log($(".firstHeading", html).text());
+    console.log($(".bday", html).text());
+  })
+  .catch(function (err) {
+    console.log("Something went wrong...");
+    console.log(err);
+  });
